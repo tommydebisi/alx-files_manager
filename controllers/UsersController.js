a8ce65c67f5443f19f8b7ab1961396bb96d5d644
tommydebisi <tommydebisi@gmail.com>
2023-03-07 02:12:10 +0100

    :seedling: made use of return to avoid err

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,31 +4,30 @@ import dbClient from '../utils/db';
 export default async function postNew(req, res) {
   const { email, password } = req.body;
   if (!email) {
-    res.status(400)
+    return res.status(400)
       .json({ error: 'Missing email' });
   }
 
   if (!password) {
-    res.status(400)
+    return res.status(400)
       .json({ error: 'Missing password' });
   }
 
   const isPresent = await dbClient.fieldPresent('users', { email });
 
   if (isPresent) {
-    res.status(400)
+    return res.status(400)
       .json({ error: 'Already exist' });
-  } else {
-    const result = await dbClient.insertCol('users', {
-      email,
-      password: sha1(password),
-    });
-
-    const newObj = result;
-    res.status(201)
-      .json({
-        id: newObj._id,
-        email: newObj.email,
-      });
   }
+  const result = await dbClient.insertCol('users', {
+    email,
+    password: sha1(password),
+  });
+
+  const newObj = result;
+  return res.status(201)
+    .json({
+      id: newObj._id,
+      email: newObj.email,
+    });
 }
