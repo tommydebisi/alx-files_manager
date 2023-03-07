0a77247b5a66d7215b4820a7ffa7f5d64cea8f47
tommydebisi <tommydebisi@gmail.com>
2023-03-07 18:37:32 +0100

    :art: wrapped controllers in classes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,53 +3,55 @@ import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-export async function postNew(req, res) {
-  const { email, password } = req.body;
-  if (!email) {
-    return res.status(400)
-      .json({ error: 'Missing email' });
-  }
+export default class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400)
+        .json({ error: 'Missing email' });
+    }
 
-  if (!password) {
-    return res.status(400)
-      .json({ error: 'Missing password' });
-  }
+    if (!password) {
+      return res.status(400)
+        .json({ error: 'Missing password' });
+    }
 
-  const fieldObj = await dbClient.getField('users', { email });
+    const fieldObj = await dbClient.getField('users', { email });
 
-  if (fieldObj) {
-    return res.status(400)
-      .json({ error: 'Already exist' });
-  }
+    if (fieldObj) {
+      return res.status(400)
+        .json({ error: 'Already exist' });
+    }
 
-  const result = await dbClient.insertCol('users', {
-    email,
-    password: sha1(password),
-  });
-  const newObj = result;
-  return res.status(201)
-    .json({
-      id: newObj._id,
-      email: newObj.email,
+    const result = await dbClient.insertCol('users', {
+      email,
+      password: sha1(password),
     });
-}
-
-export async function getMe(req, res) {
-  const tokenKey = `auth_${req.get('X-Token')}`;
-
-  // get userId from tokenKey stored in redis database
-  const userId = await redisClient.get(tokenKey);
-
-  if (!userId) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    const newObj = result;
+    return res.status(201)
+      .json({
+        id: newObj._id,
+        email: newObj.email,
+      });
   }
 
-  // change userId gotten to ObjectId
-  const fieldObj = await dbClient.getField('users',
-    { _id: ObjectId('64062cfd5a6d34519f4ac23b') });
+  static async getMe(req, res) {
+    const tokenKey = `auth_${req.get('X-Token')}`;
 
-  return res.json({
-    id: fieldObj._id,
-    email: fieldObj.email,
-  });
+    // get userId from tokenKey stored in redis database
+    const userId = await redisClient.get(tokenKey);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // change userId gotten to ObjectId
+    const fieldObj = await dbClient.getField('users',
+      { _id: ObjectId('64062cfd5a6d34519f4ac23b') });
+
+    return res.json({
+      id: fieldObj._id,
+      email: fieldObj.email,
+    });
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
-import { getStatus, getStats } from '../controllers/AppController';
-import { postNew, getMe } from '../controllers/UsersController';
-import { getConnect, getDisconnect } from '../controllers/AuthController';
-import postUpload from '../controllers/FilesController';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.route('/status').get(getStatus);
-router.route('/stats').get(getStats);
-router.route('/users/me').get(getMe);
-router.route('/connect').get(getConnect);
-router.route('/disconnect').get(getDisconnect);
-router.route('/users').post(postNew);
-router.route('/files').post(postUpload);
+router.route('/status').get(AppController.getStatus);
+router.route('/stats').get(AppController.getStats);
+router.route('/users/me').get(UsersController.getMe);
+router.route('/connect').get(AuthController.getConnect);
+router.route('/disconnect').get(AuthController.getDisconnect);
+router.route('/users').post(UsersController.postNew);
+router.route('/files').post(FilesController.postUpload);
 
 export default router;
