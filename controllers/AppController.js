9e16133feb6b979993ea4e8294d21c65d7ebccf1
tommydebisi <tommydebisi@gmail.com>
2023-03-09 12:37:07 +0100

    :seedling: removed class in app control

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,22 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
-  static getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
+export function getStatus(req, res) {
+  const redisAlive = redisClient.isAlive();
+  const dbAlive = dbClient.isAlive();
 
-    res.status(200).json({
-      redis: redisAlive,
-      db: dbAlive,
-    });
-  }
+  res.status(200).json({
+    redis: redisAlive,
+    db: dbAlive,
+  });
+}
 
-  static async getStats(req, res) {
-    const userCount = await dbClient.nbUsers();
-    const fileCount = await dbClient.nbFiles();
+export async function getStats(req, res) {
+  const userCount = await dbClient.nbUsers();
+  const fileCount = await dbClient.nbFiles();
 
-    res.status(200).json({
-      users: userCount,
-      files: fileCount,
-    });
-  }
+  res.status(200).json({
+    users: userCount,
+    files: fileCount,
+  });
 }
