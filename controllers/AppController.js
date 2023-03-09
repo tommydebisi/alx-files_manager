21b2132d9a345c47f3724468c178af917444e049
tommydebisi <tommydebisi@gmail.com>
2023-03-09 12:23:08 +0100

    :seedling: removed connection check for appcontr

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,24 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export function getStatus(req, res) {
-  const redisAlive = redisClient.isAlive();
-  const dbAlive = dbClient.isAlive();
+export default class AppController {
+  static getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
 
-  res.status(200).json({
-    redis: redisAlive,
-    db: dbAlive,
-  });
-}
+    res.status(200).json({
+      redis: redisAlive,
+      db: dbAlive,
+    });
+  }
 
-export async function getStats(req, res) {
-  const userCount = await dbClient.nbUsers();
-  const fileCount = await dbClient.nbFiles();
+  static async getStats(req, res) {
+    const userCount = await dbClient.nbUsers();
+    const fileCount = await dbClient.nbFiles();
 
-  res.status(200).json({
-    users: userCount,
-    files: fileCount,
-  });
+    res.status(200).json({
+      users: userCount,
+      files: fileCount,
+    });
+  }
 }
